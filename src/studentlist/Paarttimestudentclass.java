e9d2451e214c57fe464346300d969cc016f9c5f2
Tirath015 <paddati@sheridancollege.ca>
2025-01-22 09:40:31 -0500

    added one more class like parttimestudent

---
+++ src/studentlist/Paarttimestudentclass.java
@@ -0,0 +1,15 @@
+package studentlist;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author tirat
+ */
+public class Paarttimestudentclass extends Student{
+    public int numofcourse;
+    
+}
